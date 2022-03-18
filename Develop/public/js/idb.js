3ea04e283bdd0719c8ae7d5da553259c00cf83af
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2022-03-18 12:49:47 -0400

    setting up idb.js

---
+++ Develop/public/js/idb.js
@@ -0,0 +1,23 @@
+// this variable should hold the db connection
+let db;
+
+// establish connection to indexedDB database 
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+
+    db.createObjectStore('new_budget', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadBudget();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
