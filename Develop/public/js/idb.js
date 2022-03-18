2490a27516a820f71cac605cdb73d85b10962305
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2022-03-18 13:10:39 -0400

    added idb functionality to take records when offline and submit when user is back online

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -1,3 +1,4 @@
+
 // this variable should hold the db connection
 let db;
 
@@ -10,14 +11,62 @@ request.onupgradeneeded = function(event) {
     db.createObjectStore('new_budget', { autoIncrement: true });
 };
 
+// on success
 request.onsuccess = function(event) {
     db = event.target.result;
 
     if (navigator.onLine) {
-        uploadBudget();
+        // need to create uploadBudget();
     }
 };
 
 request.onerror = function(event) {
     console.log(event.target.errorCode);
 };
+
+// function to be started if there is no internet connection and we try to submit new transaction
+function saveRecord(record) {
+
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+    // this will add the record to the object store
+    budgetObjectStore.add(record);
+}
+
+function uploadTransaction() {
+    
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+    // gets and sets all records to the variable getRecords
+    const getRecords = budgetObjectStore.getRecords();
+
+    getRecords.onsuccess = function() {
+        // if theres something in the indexeddbs store it should be sent to api server
+        if (getRecords.result.length > 0) {
+            fetch('api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getRecords.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+            const transaction = db.transaction(['new_transaction'], 'readwrite');
+            const budgetObjectStore = transaction.objectStore('new_transaction');
+            // will clear all items in the store
+            budgetObjectStore.clear();
+            alert('All your saved transactions have been sent');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    }
+}
+
+window.addEventListener('online', uploadTransaction);
